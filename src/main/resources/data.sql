b335547f55f03f3bc9da16c77c75b25a477bbc94
RichaVPatel <richu2593@gmail.com>
2020-03-29 17:51:34 -0400

    Added Student class for Jdbc Template demo with data and schema.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into student
+values (10001, 'Richa', 'Patel');
+
+insert into student
+values (10002, 'Vivek', 'Patel');
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,6 @@
+create table student
+(
+    id integer not null ,
+    name varchar(255) not null ,
+    passport_number varchar(255) not null
+);
